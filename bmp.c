904b6dff113cc75f3fe5b0c03e7220e2d663e4cd
Alrick EMILIEN <aemilien@e3r7p23.42.fr>
2017-01-05 13:45:23 +0100

    [DEBUG][OK] bitmap OK, dois etre mis a la norme

--- bmp.c
+++ bmp.c
@@ -6,23 +6,30 @@
 void	set_bitmapfileheader(int fd, t_image *image)
 {
 	unsigned char	*header;
-	unsigned int		file_size;
+	unsigned int	file_size;
 	unsigned char	*tmp;
 	int		i;
-	unsigned char c;
+//	unsigned char c;
 	int ret;
+	int		len;
 
 	header = (unsigned char*)malloc(sizeof(unsigned char) * 14);
 	ft_bzero(header, 14);
 	header[0] = 0x42;
 	header[1] = 0x4D;
-	file_size = image->size_line * image->height;
+
+	len = image->height * image->height * 3;
+	i = 0;
+	while ((len + i) % 4)
+		i++;
+	len += i;
+	file_size = len + 14 + 40;
 	tmp = (unsigned char*)&file_size;
-	printf("tmp[1] %x\n\n", tmp[2]);
-	header[2] = tmp[3];
-	header[3] = tmp[2];
-	header[4] = tmp[1];
-	header[5] = tmp[0];
+	//printf("file size %x\n\n", file_size);
+	header[2] = tmp[0];
+	header[3] = tmp[1];
+	header[4] = tmp[2];
+	header[5] = tmp[3];
 /*
  * offset : Adresse de la zone de définition de l’image
  */
@@ -30,13 +37,13 @@ void	set_bitmapfileheader(int fd, t_image *image)
 	//header[11] = 0;
 	//header[12] = 0;
 	//header[13] = 0;
-	i = 0;
-	while (i < 6)
+	/*i = 0;
+	while (i < 14)
 	{
 		c= header[i];
 		printf("%X\n", c);
 		i++;
-	}
+	}*/
 	ret = write(fd, header, 14);
 	/*printf("ret : %d\n", ret);
 	close (fd);
@@ -47,42 +54,44 @@ void	set_bitmapfileheader(int fd, t_image *image)
 		printf("%X\n", c);
 		i++;
 	}*/
-	perror("write error");
-	ft_putchar('\n');
+	if (ret == -1)
+		perror("write error");
+	//ft_putchar('\n');
 }
 
 void	set_bitmapinfoheader(int fd, t_image *image)
 {
-	char	*header;
-	char	*tmp;
-	int		n;
-	
-	header = (char*)malloc(sizeof(char) * 40);
+	unsigned char	*header;
+	unsigned char	*tmp;
+	unsigned int		n;
+	int		i;
+
+	header = (unsigned char*)malloc(sizeof(unsigned char) * 40);
 	ft_bzero(header, 40);
 	
 	header[0] = 0x28;
 
 	n = image->width;
-	tmp = (char*)&n;
-	header[4] = tmp[3];
-	header[5] = tmp[2];
-	header[6] = tmp[1];
-	header[7] = tmp[0];
+	tmp = (unsigned char*)&n;
+	header[4] = tmp[0];
+	header[5] = tmp[1];
+	header[6] = tmp[2];
+	header[7] = tmp[3];
 	
 	n = image->height;
-	tmp = (char*)&n;
-	header[8] = tmp[3];
-	header[9] = tmp[2];
-	header[10] = tmp[1];
-	header[11] = tmp[0];
+	tmp = (unsigned char*)&n;
+	header[8] = tmp[0];
+	header[9] = tmp[1];
+	header[10] = tmp[2];
+	header[11] = tmp[3];
 
 	header[12] = 0x01;
 	//header[13] = 0;
 	
 	n = image->bits_per_pixel;
-	tmp = (char*)&n;
-	header[14] = tmp[1];
-	header[15] = tmp[0];
+	tmp = (unsigned char*)&n;
+	header[14] = 0x18; // !!!!!!!!!!!!!
+	header[15] = 0;
 
 /*compression
 	header[16]
@@ -90,12 +99,13 @@ void	set_bitmapinfoheader(int fd, t_image *image)
 	header[18]
 	header[19]
 */	
-	n = image->size_line * image->height;
-	tmp = (char*)&n;
-	header[20] = tmp[3];
-	header[21] = tmp[2];
-	header[22] = tmp[1];
-	header[23] = tmp[0];
+	n = image->height * image->width * 3;
+	printf("bpp : %d\n", n);
+	tmp = (unsigned char*)&n;
+	header[20] = tmp[0];
+	header[21] = tmp[1];
+	header[22] = tmp[2];
+	header[23] = tmp[3];
 
 	header[24] = 0xC3;
 	header[25] = 0x0E;
@@ -115,7 +125,15 @@ void	set_bitmapinfoheader(int fd, t_image *image)
 	//header[37]
 	//header[38]
 	//header[39]
+	
+	i = 0;
+	while (i < 40)
+	{
+		printf("%X\n", header[i]);
+		i++;
+	}
 	write(fd, header, 40);
+	ft_putchar ('\n');
 }
 
 void	set_bitmapdata(int fd, t_image *image)
@@ -126,9 +144,15 @@ void	set_bitmapdata(int fd, t_image *image)
 	int		line;
 	int		colonne;
 	int		i;
+	int		len;
 
-	bitmap = (char*)malloc(sizeof(char) * image->size_line * image->height);
-	ft_bzero(bitmap, sizeof(char) * image->size_line * image->height);
+	len = image->height * image->height * 3;
+	i = 0;
+	while ((len + i) % 4)
+		i++;
+	len += i;
+	bitmap = (char*)malloc(sizeof(char) * len);
+	ft_bzero(bitmap, sizeof(char) * len);
 	y = image->height - 1;
 	i = 0;
 	while (y >= 0)
@@ -146,7 +170,7 @@ void	set_bitmapdata(int fd, t_image *image)
 		}
 		y--;
 	}
-	write(fd, bitmap, sizeof(char) * image->size_line * image->height);
+	write(fd, bitmap, sizeof(char) * len);
 }
 
 void	ft_bitmap(t_image *image)
@@ -155,16 +179,17 @@ void	ft_bitmap(t_image *image)
 	unsigned char	c;
 	int		i;
 
-	fd = open("screenshot.bmp", O_RDWR | O_EXCL | O_CREAT | O_NONBLOCK , S_ISCHR);
+	fd = open("screenshot.bmp", O_RDWR | O_EXCL | O_CREAT | O_NONBLOCK, S_IRUSR | S_IWUSR);
 	set_bitmapfileheader(fd, image);
 	set_bitmapinfoheader(fd, image);
 	set_bitmapdata(fd, image);
 	close (fd);
-	fd = open("screenshot.bmp", O_RDWR | O_NONBLOCK, S_ISCHR);
+	fd = open("screenshot.bmp", O_RDWR | O_NONBLOCK, S_IRUSR | S_IWUSR);
 	i = 0;
-	while (read(fd, &c, 1) && i < 14)
+	while (read(fd, &c, 1) && i < 54)
 	{
-		printf("%X\n", c);
+		if (i >= 14)
+			printf("%X\n", c);
 		i++;
 	}
 	ft_putendl("OK");
