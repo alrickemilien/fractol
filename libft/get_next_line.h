70cf8ddb00e3bfd0388d02c46847db84b8b1631e
Alrick EMILIEN <aemilien@e2r8p20.42.fr>
2017-01-12 11:32:49 +0100

    [NORME] norme OK for libft

--- libft/get_next_line.h
+++ libft/get_next_line.h
@@ -1,22 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aemilien <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/01/12 11:32:01 by aemilien          #+#    #+#             */
+/*   Updated: 2017/01/12 11:32:18 by aemilien         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
-#define GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
-#include <sys/types.h>
-#include <sys/uio.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include "libft.h"
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include "libft.h"
 
-#define BUFF_SIZE 1
+# define BUFF_SIZE 1024
 
-// BUF_SIZE peut changer
-
-int		get_next_line(const int fd, char **line);
-
-typedef  struct	s_file
+typedef struct	s_file
 {
-	int				fd;
-	char			*buf;
+	int			fd;
+	char		*buf;
 }				t_file;
+
+int				get_next_line(const int fd, char **line);
 
 #endif
